cd178373c09281fa5bc467dca1427deed7c55b76
Mikael Korpela <mikael@ihminen.org>
2018-04-27 10:10:19 +0300

    Add `ABSPATH` test to `global-conditionals.php`

--- global-conditionals.php
+++ global-conditionals.php
@@ -4,6 +4,8 @@
  * Bridges a few Jetpack conditional functions to be global, so that Query Monitor can test them
  */
 
+defined( 'ABSPATH' ) or die();
+
 if ( class_exists( 'Jetpack' ) ) {
 	if ( method_exists( 'Jetpack', 'is_active' ) && ! function_exists( 'qm_jetpack_is_active' ) ) {
 		function qm_jetpack_is_active() {
